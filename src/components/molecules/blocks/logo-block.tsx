c061f72d2aae6b4bb5c5b7c9e51c0bd492afed55
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2023-10-03 22:33:08 +1000

    added: global style

--- src/components/molecules/blocks/logo-block.tsx
+++ src/components/molecules/blocks/logo-block.tsx
@@ -2,16 +2,18 @@
 
 import React from 'react'
 import Image from 'next/image'
-import { ProtostarDarkLogo, ProtostarLightLogo } from '@/assets/images'
+import { useTheme } from 'next-themes'
 import { useScreenSize } from '@/hooks/index'
+import { ProtostarDarkLogo, ProtostarLightLogo } from '@/assets/images'
 
 const LogoBlock = () => {
+  const { theme } = useTheme()
   const sm = useScreenSize('sm')
 
   return (
     <div className='flex items-center w-fit'>
       <Image
-        src={ProtostarDarkLogo}
+        src={theme === 'dark' ? ProtostarDarkLogo : ProtostarLightLogo}
         width={40}
         height={40}
         alt='Protostar'
@@ -19,7 +21,7 @@ const LogoBlock = () => {
         className='w-auto h-auto'
       />
       {sm && (
-        <p className='font-oxanium text-3xl font-semibold text-BLUE-midnight dark:text-WHITE-monotone ml-2'>
+        <p className='font-oxanium text-3xl font-semibold text-BLUE-midnight dark:text-WHITE-monotone ml-2 pt-1'>
           Protostar
         </p>
       )}
